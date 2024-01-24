7ef1fb8548fa2161b46400d8f90f6d46c307eda3
J3rry <jeremydix17@gmail.com>
2024-01-24 14:45:22 +0100

    updated initial spring positions

--- Simulations/OpenProjectSimulator.cpp
+++ Simulations/OpenProjectSimulator.cpp
@@ -559,8 +559,8 @@ void OpenProjectSimulator::setupPlayerPlatforms()
         player_rackets[0].platform = this->create_and_query_rigid_body(Vec3(1, 2, 1), 1, false);
         player_rackets[0].platform->warp(Vec3(0, heightPos, OFFSET_HEAT_GRID), Quat(0, 0, 0, 1));
     
-        int m1 = create_masspoint(normal_walls[0]->center_of_mass, 1);
-        int m2 = create_masspoint(player_rackets[0].platform->center_of_mass, 1);
+        int m1 = create_masspoint(Vec3(goals[0]->center_of_mass.x + goals[0]->size.x / 2, goals[0]->center_of_mass.y, goals[0]->center_of_mass.z), 0);
+        int m2 = create_masspoint(Vec3(player_rackets[0].platform->center_of_mass.x - player_rackets[0].platform->size.x / 2, player_rackets[0].platform->center_of_mass.y, player_rackets[0].platform->center_of_mass.z) , 1);
         player_rackets[0].spring = springs + create_spring(m1, m2, 1, 1);
     }
     
@@ -569,8 +569,8 @@ void OpenProjectSimulator::setupPlayerPlatforms()
         player_rackets[1].platform = this->create_and_query_rigid_body(Vec3(1, 2, 1), 1, false);
         player_rackets[1].platform->warp(Vec3(heat_grid.width - 1, heightPos, OFFSET_HEAT_GRID), Quat(0, 0, 0, 1));
     
-        int m1 = this->create_masspoint(normal_walls[1]->center_of_mass, 1);
-        int m2 = this->create_masspoint(player_rackets[1].platform->center_of_mass, 1);
+        int m1 = this->create_masspoint(Vec3(goals[1]->center_of_mass.x - goals[1]->size.x / 2, goals[1]->center_of_mass.y, goals[1]->center_of_mass.z), 0);
+        int m2 = this->create_masspoint(Vec3(player_rackets[1].platform->center_of_mass.x + player_rackets[1].platform->size.x / 2, player_rackets[1].platform->center_of_mass.y, player_rackets[1].platform->center_of_mass.z), 1);
         player_rackets[1].spring = this->create_and_query_spring(m1, m2, 1, 1);
     }
 
@@ -587,7 +587,7 @@ void OpenProjectSimulator::setupBall()
     float ballScale = 1.f;
     float ballMass = 1.0f;
 
-    this->ball = this->create_and_query_rigid_body(Vec3(ballScale), ballMass, false);
+    this->ball = this->create_and_query_rigid_body(Vec3(0.75f, 0.75f, ballScale), ballMass, false);
     this->ball->warp(Vec3((heatgrid_width - heat_grid.scale) / 2, (heatgrid_width - heat_grid.scale) / 2, OFFSET_HEAT_GRID), Quat(0, 0, 0, 1));
     this->ball->set_linear_factor(Vec3(1, 1, 0));
     this->ball->set_angular_factor(Vec3(1, 1, 0));
@@ -1068,10 +1068,10 @@ void OpenProjectSimulator::draw_game() {
                     color = lerp(medium_color, cold_color, -value);
 
                 // just for debugging
-                color = Vec3(0, 0, 0);
-                if ((x + y) % 2 == 0){
-                    color = Vec3(1,1,1);
-                }
+                //color = Vec3(0, 0, 0);
+                //if ((x + y) % 2 == 0){
+                //    color = Vec3(1,1,1);
+                //}
                 this->DUC->setUpLighting(Vec3(0, 0, 0), color, 5, color);
 
                 // Draw Heat map as grid of cubes
